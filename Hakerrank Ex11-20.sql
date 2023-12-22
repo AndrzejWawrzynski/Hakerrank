16802999fad8e492e5f337e18ad995a5ec2a3540
Andrzej Wawrzyński <wandrzej@interia.pl>
2023-12-22 21:11:17 +0100

    Ex15-18

--- Hakerrank Ex11-20.sql
+++ Hakerrank Ex11-20.sql
@@ -71,7 +71,7 @@ FROM station
 WHERE city NOT REGEXP "^[aeiou]";
 
 /*
-Ex14 Weather Observation Station 9
+Ex14 Weather Observation Station 10
 Query the list of CITY names from STATION that do not end with vowels. 
 Your result cannot contain duplicates.
 */
@@ -91,4 +91,87 @@ SELECT
 	DISTINCT city 
 FROM station 
 WHERE city NOT REGEXP '[aeiou]$';
+
+/*
+Ex15 Weather Observation Station 11
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+
+SELECT
+	DISTINCT city 
+FROM station 
+WHERE (city NOT LIKE ('a%') 
+		AND city NOT LIKE ('e%') 
+		AND city NOT LIKE ('i%') 
+		AND city NOT LIKE ('o%') 
+		AND city NOT LIKE ('u%')) 
+    OR 
+		(city NOT LIKE ('%a') 
+        AND city NOT LIKE ('%e') 
+        AND city NOT LIKE ('%i') 
+        AND city NOT LIKE ('%o') 
+        AND city NOT like ('%u'));
+        
+-- OR
+
+SELECT 
+	DISTINCT city 
+FROM station 
+WHERE city NOT REGEXP '^[aeiou]' 
+	OR city NOT REGEXP '[aeiou]$';
+    
+/*
+Ex16 Weather Observation Station 12
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+
+SELECT
+	DISTINCT city 
+FROM station 
+WHERE (city NOT LIKE ('a%') 
+		AND city NOT LIKE ('e%') 
+		AND city NOT LIKE ('i%') 
+		AND city NOT LIKE ('o%') 
+		AND city NOT LIKE ('u%')) 
+    AND
+		(city NOT LIKE ('%a') 
+        AND city NOT LIKE ('%e') 
+        AND city NOT LIKE ('%i') 
+        AND city NOT LIKE ('%o') 
+        AND city NOT like ('%u'));
+        
+-- OR
+
+SELECT 
+	DISTINCT city 
+FROM station 
+WHERE city NOT REGEXP '^[aeiou]' 
+	AND city NOT REGEXP '[aeiou]$';
+    
+/*
+Ex17 Employee Names
+Write a query that prints a list of employee names (i.e.: the name attribute) 
+from the Employee table in alphabetical order.
+*/
+
+SELECT 
+	name 
+FROM employee
+ORDER BY name;
+
+/*
+Ex18
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees 
+in Employee having a salary greater than 2000$ per month who have been employees for less than 10 months. 
+Sort your result by ascending employee_id.
+*/
+
+SELECT 
+	name 
+FROM employee
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id;
+
 
