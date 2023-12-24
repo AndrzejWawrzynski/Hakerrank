8a0dfd88ae35b5bd001c19dce8cffcebb3232cbe
Andrzej Wawrzyński <wandrzej@interia.pl>
2023-12-24 13:33:50 +0100

    update Ex20

--- Hakerrank Ex11-20.sql
+++ Hakerrank Ex11-20.sql
@@ -202,9 +202,9 @@ Not A Triangle: The given values of A, B, and C don't form a triangle.
 
 SELECT 
     CASE  
-        WHEN a + b <= c OR b + c <= a OR c + a <= b THEN 'Not A Triangle'
-        WHEN a = b AND b = c THEN 'Equilateral'
-        WHEN a = b OR b = c OR c = a THEN 'Isosceles'
+        WHEN (a=b AND b=c) AND (a+b>c AND b+c>a AND c+a>b) THEN 'Equilateral'
+        WHEN a+b<=c OR b+c<=a OR c+a<=b THEN 'Not A Triangle'
+        WHEN a=b OR b=c OR c=a THEN 'Isosceles'
         ELSE 'Scalene' 
     END  
 FROM triangles;
