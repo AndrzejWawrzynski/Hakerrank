1af8fbdecb6ec5ea0db36338ab756548ae1026f7
Andrzej Wawrzyński <wandrzej@interia.pl>
2023-12-28 17:56:22 +0100

    Ex36-39

--- Hakerrank Ex31-40.sql
+++ Hakerrank Ex31-40.sql
@@ -67,5 +67,111 @@ JOIN country
 ON city.countrycode = country.code
 WHERE country.continent LIKE 'Asia';
 
+/*
+Ex36 African Cities
+Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+SELECT
+    city.name
+FROM city
+JOIN country
+ON city.countrycode = country.code
+WHERE country.continent LIKE 'Africa';
+
+/*
+Ex37 Average Population of Each Continent
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) 
+and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+SELECT
+    country.continent,
+    FLOOR(AVG(city.population))
+FROM city
+JOIN country
+ON city.countrycode = country.code
+GROUP BY country.continent;
+
+/*
+Ex38 Draw The Triangle 1
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+* * * * *
+* * * *
+* * *
+* *
+*
+
+Write a query to print the pattern P(20).
+*/
+
+DELIMITER $$
+CREATE PROCEDURE pattern(n INT)
+BEGIN 
+    DECLARE i INT DEFAULT n;
+        WHILE i > 0 DO
+            SELECT IF (i = 0, '*', REPEAT('* ', i));
+            SET i = i - 1;
+        END WHILE;
+END $$
+DELIMITER ;
+CALL pattern(20);
+
+-- OR
+
+DELIMITER $$
+CREATE PROCEDURE pattern()
+BEGIN 
+    DECLARE i INT DEFAULT 20;
+        WHILE i > 0 DO
+            SELECT IF (i = 0, '*', REPEAT('* ', i));
+            SET i = i - 1;
+        END WHILE;
+END $$
+DELIMITER ;
+CALL pattern();
+
+/*
+Ex39 Draw The Triangle 2
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+*
+* * 
+* * *
+* * * *
+* * * * *
+Write a query to print the pattern P(20).
+*/
+
+DELIMITER $$
+CREATE PROCEDURE pattern2()
+BEGIN
+	DECLARE i INT DEFAULT 1;
+        WHILE i < 21 DO
+			SELECT REPEAT('* ', i);
+			SET i = i + 1;
+        END WHILE;
+END $$
+DELIMITER ;
+CALL pattern2()
+
+-- OR
+
+DELIMITER $$
+CREATE PROCEDURE pattern2(n INT)
+BEGIN
+	DECLARE i INT DEFAULT 1;
+        WHILE i < n DO
+			SELECT IF (i = 1, '*', REPEAT('* ', i));
+			SET i = i + 1;
+        END WHILE;
+END $$
+DELIMITER ;
+CALL pattern2(21)
+
+/*
+Ex40 
+
 
 
